6b3e97236fd6a97796092f4bb5b995062ba3d87c
Roy <roycrippen4@gmail.com>
2023-12-16 23:12:08 -0500

    added clock and date to tab bar

--- tab_bar.py
+++ tab_bar.py
@@ -14,18 +14,25 @@ from kitty.tab_bar import (
 )
 
 opts = get_options()
-# icon_fg = as_rgb(color_as_int(opts.color1))
-icon_bg = as_rgb(color_as_int(opts.color6))
 
-date_fgcolor = as_rgb(color_as_int(opts.color6))
-# date_bgcolor = as_rgb(color_as_int(opts.color9))
-date_bgcolor = as_rgb(color_as_int(opts.active_tab_background))
+LEFT_END = ""
+LEFT_END_BGCOLOR = as_rgb(color_as_int(opts.active_tab_background))
+LEFT_END_FGCOLOR = as_rgb(color_as_int(opts.tab_bar_background))
 
-separator_fg = as_rgb(color_as_int(opts.color9))
+CLOCK_SEP = " │ "
+
+CLOCK_BG = as_rgb(color_as_int(opts.color4))
+CLOCK_FG = as_rgb(color_as_int(opts.active_tab_background))
+
+DATE_BG = as_rgb(color_as_int(opts.color8))
+DATE_FG = as_rgb(color_as_int(opts.active_tab_background))
+
+# separator_fg = as_rgb(color_as_int(opts.color9))
 
 bat_text_color = as_rgb(color_as_int(opts.color15))
 SEPARATOR_SYMBOL, SOFT_SEPARATOR_SYMBOL = ("", "")
 RIGHT_MARGIN = 0
+REFRESH_TIME = 1
 
 def _draw_left_status(
     draw_data: DrawData,
@@ -93,6 +100,7 @@ def _redraw_tab_bar(_):
         tm.mark_tab_bar_dirty()
 
 
+timer_id = None
 right_status_length = -1
 
 def draw_tab(
@@ -105,9 +113,16 @@ def draw_tab(
     is_last: bool,
     extra_data: ExtraData,
 ) -> int:
+    global timer_id
     global right_status_length
-    date = datetime.now().strftime(" %d.%m.%Y")
-    cells = [(date_bgcolor, date_fgcolor, date)]
+    if timer_id is None:
+        timer_id = add_timer(_redraw_tab_bar, REFRESH_TIME, True)
+    cells = [
+        (LEFT_END_FGCOLOR, LEFT_END_BGCOLOR, LEFT_END),
+        (CLOCK_FG, CLOCK_BG, datetime.now().strftime(" %I:%M:%S")),
+        (DATE_FG, DATE_BG, CLOCK_SEP),
+        (DATE_FG, DATE_BG, datetime.now().strftime("%m/%d/%Y ")),
+    ]
     right_status_length = RIGHT_MARGIN
     for cell in cells:
         right_status_length += len(str(cell[2]))
